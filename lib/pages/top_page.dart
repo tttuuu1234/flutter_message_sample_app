0f22ca04b8b1a9235d01598239b47b33183de582
tttuuu1234 <yidongyi8@gmail.com>
2022-07-02 20:57:12 +0900

    :sparkles: 静的解析にルールの追加

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class TopPage extends StatelessWidget {
-  const TopPage({Key? key}) : super(key: key);
+  const TopPage({super.key});
 
   @override
   Widget build(BuildContext context) {
